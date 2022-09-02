5364f53a959d15354ab2cb64ebd94dbd663e212a
Ptr Alixandrovich <65368113+GhostXVlll@users.noreply.github.com>
2022-09-02 21:34:23 +0400

    Interface

--- Assets/Game/Scripts/ItemSpawner.cs
+++ Assets/Game/Scripts/ItemSpawner.cs
@@ -5,17 +5,19 @@ namespace BeatSort
 {
     public class ItemSpawner : MonoBehaviour
     {
-        [SerializeField] private GameObject _prefab;
+        [SerializeField] private GameObject[] _prefab;
         [SerializeField] private int _count;
         [SerializeField] private Vector3 _range;
         [SerializeField] private int spawnDelay = 2;
+
 
         IEnumerator Start()
         {
             for (int i = 0; i < _count; i++)
             {
+                var rand = Random.Range(0, _prefab.Length);
                 Vector3 offset = new Vector3(_range.x, _range.y, Random.Range(-_range.z, _range.z));
-                Instantiate(_prefab, transform.position + offset, Quaternion.identity, transform);
+                Instantiate(_prefab[rand], transform.position + offset, Quaternion.identity, transform);
 
                 yield return new WaitForSeconds(spawnDelay);
             }
