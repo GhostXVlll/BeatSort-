0efe1eeab3ae87c89e0897618f951f97928056de
Ptr Alixandrovich <65368113+GhostXVlll@users.noreply.github.com>
2022-09-21 18:44:47 +0400

    0.0.9
    Начал писать модель сохранения данных (3 скрипта).

---
+++ Assets/Game/Scripts/Models/PlayerModel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeatSort
+{
+    [System.Serializable]
+    public class PlayerModel : Model
+    {
+        [SerializeField] protected string _playerName;
+        [SerializeField] protected int _score;
+
+        public string PlayerName
+        {
+            get => _playerName;
+            set
+            {
+                _playerName = value;
+            }
+        }
+        public int Score
+        {
+            get => _score;
+            set
+            {
+                _score = value;
+            }
+        }
+    }
+}
---
+++ Assets/Game/Scripts/ScriptableModel/Model.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace BeatSort
+{
+    [System.Serializable]
+    public abstract class Model
+    {
+        [SerializeField] protected string _name;
+
+
+    }
+}
